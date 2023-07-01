2bbb6690b27fe022d3e3391cfcb5b028627957b2
Shivendra1702 <shivendrapratapjadia@gmail.com>
2023-07-01 09:07:29 +0530

    commit-1-7-23

--- client/src/components/customMessageForms/Ai.jsx
+++ client/src/components/customMessageForms/Ai.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import MessageFormUI from "./MessageFormUI";
+import { usePostAiTextMutation } from "@/state/api";
 
-const StandardMessageForm = ({ props, activeChat }) => {
+const Ai = ({ props, activeChat }) => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("");
+  const [trigger] = usePostAiTextMutation();
 
   const handleChange = (e) => setMessage(e.target.value);
 
@@ -24,10 +26,10 @@ const StandardMessageForm = ({ props, activeChat }) => {
     };
 
     props.onSubmit(form);
+    trigger(form); 
     setMessage("");
     setAttachment("");
   };
-
   return (
     <MessageFormUI
       setAttachment={setAttachment}
@@ -38,4 +40,4 @@ const StandardMessageForm = ({ props, activeChat }) => {
   );
 };
 
-export default StandardMessageForm;
+export default Ai;
